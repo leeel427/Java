f16ae62fbd7b524194ea5280b615e5c0cb789d2d
leeel427 <1star2gold@gmail.com>
2022-10-28 16:00:45 +0900

    Chap02

---
+++ Chap02(변수)/src/kr/co/ezenac/variable03/BooleanTest.java
@@ -0,0 +1,12 @@
+package kr.co.ezenac.variable03;
+
+public class BooleanTest {
+
+	public static void main(String[] args) {
+		
+		boolean isMarred = true;
+		System.out.println(isMarred);
+		
+	}
+
+}
